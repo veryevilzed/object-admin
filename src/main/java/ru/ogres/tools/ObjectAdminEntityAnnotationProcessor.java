110387408d9a929c2f27470bb58e45d05ec407e7
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2016-07-23 00:03:30 +0300

    Processor

--- src/main/java/ru/ogres/tools/ObjectAdminEntityAnnotationProcessor.java
+++ src/main/java/ru/ogres/tools/ObjectAdminEntityAnnotationProcessor.java
@@ -1,9 +1,21 @@
 package ru.ogres.tools;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeSpec;
+import org.springframework.data.repository.CrudRepository;
+
 import javax.annotation.processing.*;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,7 +56,49 @@ public class ObjectAdminEntityAnnotationProcessor extends AbstractProcessor {
     }
 
     private void build(Map<Type, TypeElement> finded) {
-        
+        finded.forEach((type, element) -> {
+            try {
+                String name = String.format("%sRepository", type.getClass().getName());
+                Type idType = null;
+
+                for(Field field : type.getClass().getDeclaredFields()){
+                    Annotation idAnnotation = field.getAnnotation(Id.class);
+                    if (idAnnotation != null){
+                        idType = field.getType();
+                        break;
+                    }
+                }
+
+                if (idType == null)
+                    for(Method method : type.getClass().getMethods()){
+                        Annotation idAnnotation = method.getAnnotation(Id.class);
+                        if (idAnnotation != null && method.getName().startsWith("get")){
+                            idType = method.getReturnType();
+                            break;
+                        }
+                    }
+
+                if (idType == null){
+                    messager.printMessage(Diagnostic.Kind.ERROR, name +" not found @Id annotation");
+                    return;
+                }
+
+
+                TypeSpec typeSpec = TypeSpec.interfaceBuilder(name)
+                        .addModifiers(Modifier.PUBLIC)
+                        .addSuperinterface(ParameterizedTypeName.get(CrudRepository.class, type, idType))
+                        .build();
+
+                JavaFile javaFile = JavaFile.builder("ru.ogres.tools.repo", typeSpec)
+                        .build();
+                javaFile.writeTo(filer);
+                messager.printMessage(Diagnostic.Kind.NOTE, String.format("Entity Repo %s builded.", name));
+            }catch (IOException e){
+                messager.printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+            }
+        });
+
+
     }
 
 }
