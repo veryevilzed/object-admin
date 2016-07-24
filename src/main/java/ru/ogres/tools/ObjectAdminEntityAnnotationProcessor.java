f4602d762ae67e4e9d72e83181221f37c12d72e0
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2016-07-24 10:16:34 +0300

    worked

--- src/main/java/ru/ogres/tools/ObjectAdminEntityAnnotationProcessor.java
+++ src/main/java/ru/ogres/tools/ObjectAdminEntityAnnotationProcessor.java
@@ -1,7 +1,12 @@
 package ru.ogres.tools;
 
+import com.fasterxml.classmate.types.TypePlaceHolder;
 import com.squareup.javapoet.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.processing.*;
 import javax.lang.model.element.Element;
@@ -10,18 +15,13 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
 import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Types;
-import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.tools.Diagnostic;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -73,25 +73,27 @@ public class ObjectAdminEntityAnnotationProcessor extends AbstractProcessor {
 
             for (VariableElement variableElement : ElementFilter.fieldsIn(typeElement.getEnclosedElements())) {
                 System.out.println("Field: " + variableElement.getSimpleName());
-                if ( variableElement.getAnnotation(Id.class) != null){
+                if (variableElement.getAnnotation(Id.class) != null) {
                     keyName = variableElement.getSimpleName().toString();
                     keyType = variableElement.asType();
-                    System.out.println(" Name: " + keyName + " Type:" + keyType.toString());
+                    System.out.println(" Name:" + keyName + " Type:" + keyType.toString());
                 }
             }
 
             if (keyName == null)
                 messager.printMessage(Diagnostic.Kind.ERROR, "@Id field not found");
-            build(typeElement, keyType, keyName);
+            try {
+                TypeSpec repository = buildRepository(typeElement, keyType, keyName);
+                buildWebHandler(typeElement, keyType, keyName, repository);
+            }catch (Exception e){
+                messager.printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+            }
         }
     }
 
+    private TypeSpec buildRepository(TypeElement typeElement, TypeMirror keyType, String keyName) throws IOException {
 
-    private void build(TypeElement typeElement, TypeMirror keyType, String keyName) {
-
-        try{
             String name = typeElement.getSimpleName() + "Repository";
-
             TypeSpec typeSpec = TypeSpec.interfaceBuilder(name)
                     .addModifiers(Modifier.PUBLIC)
                     .addSuperinterface(ParameterizedTypeName.get(ClassName.get(CrudRepository.class),
@@ -102,10 +104,52 @@ public class ObjectAdminEntityAnnotationProcessor extends AbstractProcessor {
             JavaFile javaFile = JavaFile.builder("ru.ogres.tools.repo", typeSpec)
                     .build();
             javaFile.writeTo(filer);
-        }catch (IOException e){
-            messager.printMessage(Diagnostic.Kind.ERROR, e.getMessage());
-        }
+            return typeSpec;
+    }
 
+    private void buildWebHandler(TypeElement typeElement, TypeMirror keyType, String keyName, TypeSpec repository) throws IOException {
+        if (repository == null)
+            return;
+
+        String name = typeElement.getSimpleName() + "WebHandler";
+
+
+        TypeSpec.Builder b = repository.toBuilder();
+
+        TypeSpec typeSpec = TypeSpec.classBuilder(name)
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Controller.class)
+                .addAnnotation(
+                        AnnotationSpec.builder(RequestMapping.class)
+                                .addMember("path", "$S", "/" + typeElement.getSimpleName().toString())
+                                .build()
+                )
+
+                .addField(
+                        FieldSpec.builder(TypeVariableName.get(repository.name), "repo", Modifier.PUBLIC)
+                                .addAnnotation(Autowired.class)
+                                .build()
+                )
+                .addMethod(
+                        MethodSpec.methodBuilder("list")
+                        .addModifiers(Modifier.PUBLIC)
+                        .addAnnotation(ResponseBody.class)
+                        .addAnnotation(
+                                AnnotationSpec.builder(RequestMapping.class)
+                                        .addMember("path", "$S", "")
+                                        .build()
+                        )
+                        .returns( ParameterizedTypeName.get(ClassName.get("java.util", "List"), ClassName.get(typeElement) ))
+                        .addCode("List<$T> res = new $T<>();\n" +
+                                "repo.findAll().forEach(res::add);\n" +
+                                "return res;\n", typeElement, ArrayList.class)
+                        .build()
+                )
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("ru.ogres.tools.repo", typeSpec)
+                .build();
+        javaFile.writeTo(filer);
 
 
     }
